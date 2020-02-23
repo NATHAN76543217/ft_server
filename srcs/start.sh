7381542aa0d09d9d970592c67198f447bd0abe34
Lucie Fortin <NATHAN76543217@github.com>
2020-02-23 20:03:06 +0100

    test

--- srcs/start.sh
+++ srcs/start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-service mysql start
-sed -i -r 's/index index.html index.htm index.nginx-debian.html/index index.html index.htm index.nginx-debian.html index.php/g' /etc/nginx/sites-enabled/default
-sed -i -r 's|        #location ~ \.php$ {|        location ~ \.php$ {|g' /etc/nginx/sites-enabled/default
-sed -i -r 's|       #       fastcgi_pass unix:/run/php/php7.3-fpm.sock;|              fastcgi_pass unix:/run/php/php7.3-fpm.sock;|g' /etc/nginx/sites-enabled/default
-while true; do sleep 1000; done
+#service mysql start
+#sed -i -r 's/index index.html index.htm index.nginx-debian.html/index index.html index.htm index.nginx-debian.html index.php/g' /etc/nginx/sites-enabled/default
+#sed -i -r 's|        #location ~ \.php$ {|        location ~ \.php$ {|g' /etc/nginx/sites-enabled/default #/c/User/lucie/Desktop/nathan/programmation/ft_server/srcs/config
+sed -i -r 's|        #location ~ \.php$ \{|        location ~ \.php$ \{|g' ~/Desktop/nathan/programmation/ft_server/srcs/config
+#sed -i -r 's|       #       fastcgi_pass unix:/run/php/php7.3-fpm.sock;|              fastcgi_pass unix:/run/php/php7.3-fpm.sock;|g' /etc/nginx/sites-enabled/default
+#sed -i -r 's|       #       fastcgi_pass unix:/run/php/php7.3-fpm.sock;|              fastcgi_pass unix:/run/php/php7.3-fpm.sock;|g' /c/User/lucie/Desktop/nathan/programmation/ft_server/srcs/config
+#while true; do sleep 1000; done
