6049e99ba3646efa1cc62d5d63dfd9cf500ad1d9
Nathan Lecaille <nlecaill@z1r2p3.le-101.fr>
2020-02-25 15:10:59 +0100

    php work

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,10 @@ RUN apt-get update
 RUN apt-get -y upgrade
 RUN apt-get install -y nginx
 RUN apt-get install -y mariadb-server
-RUN apt-get install -y php-fpm php-mysql
+RUN apt-get install -y php php-common php-fpm php-mysql php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip
+RUN apt-get install -y vim
 
 COPY ./srcs/start.sh ./app/
+COPY ./srcs/default ./etc/nginx/sites-enabled/default
+#COPY ./srcs/doc.html ./var/www/html/doc.html
 CMD ["/bin/bash", "/app/start.sh"] +
--- srcs/start.sh
+++ srcs/start.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 service mysql start
-sed -i -r 's/index index.html index.htm index.nginx-debian.html/index index.html index.htm index.nginx-debian.html index.php/g' /etc/nginx/sites-enabled/default
-sed -i -r 's|        #location ~ \.php$ {|        location ~ \.php$ {|g' /etc/nginx/sites-enabled/default
-sed -i -r 's|       #       fastcgi_pass unix:/run/php/php7.3-fpm.sock;|              fastcgi_pass unix:/run/php/php7.3-fpm.sock;|g' /etc/nginx/sites-enabled/default
+#sed -i -r 's/index index.html index.htm index.nginx-debian.html/index index.html index.htm index.nginx-debian.html index.php/g' /etc/nginx/sites-enabled/default
+#sed -i 's/\t#location ~ \\.php\$ {/\tlocation ~ \\.php$ {/g' /etc/nginx/sites-enabled/default
+#sed -i 's|#\tfastcgi_pass unix:/run/php/php7.3-fpm.sock;|\tfastcgi_pass unix:/run/php/php7.3-fpm.sock;\n}|g' /etc/nginx/sites-enabled/default
+nginx -t
+service nginx restart
+echo "coucou"
+
+service php7.3-mysql start
+service php7.3-fpm start
+echo "PHP start"
+
+#touch /var/www/html/index.php
+#echo "<?php phpinfo();" >> /var/www/html/index.php
+#cat /var/www/html/index.php
 while true; do sleep 1000; done
